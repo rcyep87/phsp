cba313945418059727d776c82d96556d4991e739
Ryan Yep <ryan.yep@gmail.com>
2015-09-28 18:04:32 -0500

    created school, teacher, student, course resources prior to migration

---
+++ db/migrate/20150928230236_create_courses.rb
@@ -0,0 +1,11 @@
+class CreateCourses < ActiveRecord::Migration
+  def change
+    create_table :courses do |t|
+      t.string :name
+      t.integer :teacher_id
+      t.belongs_to :teacher, index: true, foreign_key: true
+
+      t.timestamps null: false
+    end
+  end
+end
---
+++ db/migrate/20150928230259_create_students.rb
@@ -0,0 +1,12 @@
+class CreateStudents < ActiveRecord::Migration
+  def change
+    create_table :students do |t|
+      t.string :first_name
+      t.string :last_name
+      t.integer :teacher_id
+      t.belongs_to :teacher, index: true, foreign_key: true
+
+      t.timestamps null: false
+    end
+  end
+end
