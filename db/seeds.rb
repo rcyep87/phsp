248b70f7efdbf907200c0216129b1f2b6b9908ce
Ryan Yep <ryan.yep@gmail.com>
2015-09-28 18:30:42 -0500

    created seed file - creates 50 schools, teachers, courses and 7000 students

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,20 @@
 50.times do
-  
+  school = School.create({
+      name:          Faker::University.name })
+
+  teacher = Teacher.create({
+      first_name:    Faker::Name.first_name,
+      last_name:     Faker::Name.last_name,
+      school_id:     school.id })
+
+  course = Course.create({
+    name:            Faker::Commerce.department,
+    teacher_id:      teacher.id })
+end
+
+7000.times do
+  Student.create({
+    first_name:      Faker::Name.first_name,
+    last_name:       Faker::Name.last_name,
+    teacher_id:      Faker::Number.between(1, 50) })
 end
