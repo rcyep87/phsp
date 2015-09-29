43b2be3004796550faa5ec323fe8cfca552d485e
Ryan Yep <ryan.yep@gmail.com>
2015-09-28 19:07:45 -0500

    Show action working for teacher_controller but it doesn't work when you try to click on the teacher's name

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -4,4 +4,11 @@ class TeachersController < ApplicationController
     render template: 'teachers/index.html.erb', locals: { teachers: Teacher.all }
   end
 
+  def show
+    if Teacher.exists?(params[:id])
+      teacher = Teacher.find(params[:id])
+      render template: 'teachers/show.html.erb', locals: { teacher: teacher }
+    end
+  end
+
 end
