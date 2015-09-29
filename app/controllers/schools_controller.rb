3c62643f985fc2e4acca1c7be64ba53b37ce7d86
Ryan Yep <ryan.yep@gmail.com>
2015-09-29 14:48:16 -0500

    working show for schools table

--- app/controllers/schools_controller.rb
+++ app/controllers/schools_controller.rb
@@ -2,4 +2,11 @@ class SchoolsController < ApplicationController
   def index
     render template: 'schools/index.html.erb', locals: { school_list: School.all }
   end
+
+  def show
+    if School.exists?(params[:id])
+      school = School.find(params[:id])
+      render template: 'schools/show.html.erb', locals: { school: school }
+    end
+  end
 end
